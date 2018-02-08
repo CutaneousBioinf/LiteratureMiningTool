955f5f1aba36f25a3a70b9e963ee6423a8876fc1
nmsaleh <33843909+nmsaleh@users.noreply.github.com>
2018-02-07 19:27:07 -0500

    Update LocalPubmedDatabaseCompiler.java

--- ConceptMap/PubMedRetriever/src/edu/uom/med/localPubmedDatabaseCompiler/LocalPubmedDatabaseCompiler.java
+++ ConceptMap/PubMedRetriever/src/edu/uom/med/localPubmedDatabaseCompiler/LocalPubmedDatabaseCompiler.java
@@ -6,11 +6,14 @@ import java.io.IOException;
  * PubMed database consists of >28 million biomedical articles. It is the largest repository available freely.
  * Text Mining and Natural Language Processing (NLP) researches use the entire database in order to retrieve
  * the biomedical information of interest. However, it is impossible to access the entire database online and it is 
- * prohibited. PubMed provided all the published articles through ftp for mining researches. Thus the entire database 
+ * prohibited. PubMed provides all the published articles through ftp for mining researches. Thus, the entire database 
  * can be downloaded and saved locally. We present a Java Project that can compress the downloaded articles into a
- * binary file. The project consists of two tasks that can be used in combination or separately from the wrapper class:
- * LocalPubmedDatabaseCompiler.java. The two tasks are: (1) decompressing the zip files downloaded from PubMed ftp and
- * (2) compressing and indexing of PubMed articles.
+ * binary file. 
+ * 
+ * The project consists of two tasks that can be used in combination or separately from the wrapper class:
+ * LocalPubmedDatabaseCompiler.java. The two tasks are: 
+ * 	(1) decompressing the zip files downloaded from PubMed ftp, and
+ * 	(2) compressing and indexing of PubMed articles
  * 
  * 
  * @author Kalpana Raja and Troy Cao
